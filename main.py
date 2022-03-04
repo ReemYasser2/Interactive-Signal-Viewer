f74af2d1656bee771d39e1664d6ca110f3ff047a
hassan141 <hassansamy491@gmail.com>
2022-03-04 18:22:52 +0200

    sds

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
-from PyQt5 import QtWidgets, uic
-from pyqtgraph import PlotWidget
+from PyQt5 import QtWidgets, QtCore,uic
+from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
-import sys
-
+import sys  # We need sys so that we can pass argv to QApplication
+import os
+from random import randint
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
         #Load the UI Page
-        uic.loadUi('dsp1.ui', self)
+        uic.loadUi('dsp2.ui', self)
+        
+        self.x = list(range(100))  # 100 time points
+        self.y = [randint(0,100) for _ in range(100)]  # 100 data points
 
 
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    main = MainWindow()
-    main.show()
-    sys.exit(app.exec_())
+        pen = pg.mkPen(color=(255, 0, 0))
+        self.data_line =  self.signals_plot_widget.plot(self.x, self.y, pen=pen)
 
-if __name__ == '__main__':
-    main()
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(50)
+        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.start()
+
+    def update_plot_data(self):
+        self.x = self.x[1:]  # Remove the first y element.
+        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+
+        self.y = self.y[1:]  # Remove the first
+        self.y.append(randint(0,100))  # Add a new random value.
+
+        self.data_line.setData(self.x, self.y)  # Update the data.
+
+
+app = QtWidgets.QApplication(sys.argv)
+w = MainWindow()
+w.show()
+sys.exit(app.exec_())
+        
+
+
