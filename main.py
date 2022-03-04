14e2e8f9e20f065c2c8b161face16d7726ff3150
Reem Yasser <76207497+ReemYasser2@users.noreply.github.com>
2022-03-04 18:34:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,19 @@ class MainWindow(QtWidgets.QMainWindow):
         self.timer.setInterval(50)
         self.timer.timeout.connect(self.update_plot_data)
         self.timer.start()
+        # defining lists of amplitude and time for all file types 
+        self.x_txt = []
+        self.y_txt = []
+        self.x_csv = []
+        self.y_csv = []
+        self.x_xls = []
+        self.y_xls = []
+        # defining stats variables
+        self.mean = 0
+        self.std_dev = 0
+        self.min_amplitude = 0
+        self.max_amplitude = 0
+        self.duration = 0
 
     def update_plot_data(self):
         self.x = self.x[1:]  # Remove the first y element.
@@ -32,6 +45,35 @@ class MainWindow(QtWidgets.QMainWindow):
         self.y.append(randint(0,100))  # Add a new random value.
 
         self.data_line.setData(self.x, self.y)  # Update the data.
+        
+    def browse_files(self): # Browsing files function
+        files_name = QtWidgets.QFileDialog.getOpenFileName(self, 'Open only txt or CSV or xls', os.getenv('HOME'),
+                                            "csv(*.csv);; text(*.txt) ;; xls(*.xls)")
+        path = files_name[0]
+        if pathlib.Path(path).suffix == ".txt":
+            data = np.genfromtxt(path, delimiter=',')
+            x = data[:, 0]
+            y= data[:, 1]
+            self.x_txt = list(x[:])
+            self.y_txt = list(y[:])
+           
+
+        elif pathlib.Path(path).suffix == ".csv":
+            data = np.genfromtxt(path, delimiter=' ')
+            x = data[:, 0]
+            y = data[:, 1]
+            self.x_csv= list(x[:])
+            self.y_csv = list(y[:])
+            print(self.x_csv)
+            print(self.y_csv)
+            
+
+        elif pathlib.Path(path).suffix == ".xls":
+            data = np.genfromtxt(path, delimiter=',')
+            x = data[:, 0]
+            y = data[:, 1]
+            self.x_xls = list(x[:])
+            self.y_xls = list(y[:])
 
 
 app = QtWidgets.QApplication(sys.argv)
