176cd726f72af00b8b1259051585c6b8888acffd
Reem Yasser <76207497+ReemYasser2@users.noreply.github.com>
2022-03-05 23:59:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,23 +5,33 @@ import sys  # We need sys so that we can pass argv to QApplication
 import os
 import pathlib
 import numpy as np
-
+import matplotlib.pyplot as plt
+from ui import Ui_MainWindow
+import statistics
 
 
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+        self.ui = Ui_MainWindow() 
+        self.ui.setupUi(self)
+
+        self.ui.Browse_Button.clicked.connect(self.browse_files)
+        # initializing the plot
+        x = []
+        y = []
+        pen = pg.mkPen(color=(255, 255, 255))
+        self.init_plot = self.ui.signals_plot_widget.plot(x,y,pen=pen)
 
         # Load the UI Page
-        uic.loadUi('dsp2.ui', self)
+        #uic.loadUi('dsp2.ui', self)
         # -----------
-        self.timer = QtCore.QTimer()
-        self.timer.setInterval(50)
-        self.timer.timeout.connect(self.update_plot_data)
-        self.timer.start()
-        self.Browse_Button.clicked.connect(self.browse_files)
         
+        # defining variables for plot
+        self.counter = 0
+        self.max_time = 0.12
+        self.max_time_init= 0
         # defining lists of amplitude and time for all file types
         self.x_txt = []
         self.y_txt = []
@@ -35,24 +45,16 @@ class MainWindow(QtWidgets.QMainWindow):
         self.min_amplitude = 0
         self.max_amplitude = 0
         self.duration = 0
-        #self.show()
+        
 
-    def update_plot_data(self):
-        # self.x = list(range(100))  # 100 time points
-        # self.y = [randint(0, 100) for _ in range(100)]  # 100 data points
-
-        pen = pg.mkPen(color=(0, 255, 0))
-        self.data_line = self.signals_plot_widget.plot(self.x_csv, self.y_csv, pen=pen)
-
-        #self.x = self.x[1:]  # Remove the first x element.
-        #self.x_csv.append(self.x_csv[-1] + 1)  # Add a new value 1 higher than the last.
-
-        #self.y = self.y[1:]  # Remove the first y element.
-        #self.y_csv.append(self.y_csv[-1] + 1)  # Add a new random value.
-
-        self.data_line.setData(self.x_csv, self.y_csv)  # Update the data.
 
     def browse_files(self):  # Browsing files function
+        self.counter = 0
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(100)
+        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.start()
+        
         files_name = QtWidgets.QFileDialog.getOpenFileName(self, 'Open only txt or CSV or xls', os.getenv('HOME'),
                                                            "csv(*.csv);; text(*.txt) ;; xls(*.xls)")
         path = files_name[0]
@@ -69,8 +71,6 @@ class MainWindow(QtWidgets.QMainWindow):
             y = data[:, 1]
             self.x_csv = list(x[:])
             self.y_csv = list(y[:])
-            print(self.x_csv)
-            print(self.y_csv)
             self.update_plot_data
 
         elif pathlib.Path(path).suffix == ".xls":
@@ -79,9 +79,44 @@ class MainWindow(QtWidgets.QMainWindow):
             y = data[:, 1]
             self.x_xls = list(x[:])
             self.y_xls = list(y[:])
+        
+        self.ui.signals_plot_widget.setYRange(min(self.y_csv), max(self.y_csv))
+        self.max_time = 0.12
+        
+
+    def update_plot_data(self):
+
+        # min_amplitude = min(self.y_csv)
+        # max_amplitude = max(self.y_csv)
+        # self.ui.signals_plot_widget.setYRange(min_amplitude, max_amplitude)
+        if self.counter == 0 :
+            self.ui.signals_plot_widget.setXRange(0 , self.max_time)
+        elif self.counter >= 450:
+            self.ui.signals_plot_widget.setXRange(self.max_time_init, self.max_time)
+            self.max_time = self.max_time + 0.01
+            self.max_time_init = self.max_time_init + 0.01
+        
+        
+        #self.plt = self.ui.signals_plot_widget.plot(self.x_csv, self.y_csv)
     
+        pen = pg.mkPen(color=(255, 255, 255))
+        self.init_plot.setData(self.x_csv[0:self.counter], self.y_csv[0:self.counter], pen= pen)
+        #self.plt = self.ui.signals_plot_widget.plot(self.x_csv[0:self.counter], self.y_csv[0:self.counter], pen= pen)
+        self.counter = self.counter + 1
+        #print(self.y_csv)
+        # print(self.y_csv[0:self.counter])
+        
 
+        
 
+        #self.plt = self.ui.signals_plot_widget.plot(self.x_csv, self.y_csv)
+        # if self.counter > 20: #max(self.x_csv):
+        #     self.timer.stop()
+        #     self.counter= 0
+        #     self.max_time = 20
+        #     self.max_time_init = 0
+           
+    
 app = QtWidgets.QApplication(sys.argv)
 w = MainWindow()
 w.show()
